92f9be6771264ec4ea47118711ada7c68269ee1e
aries <906130960@qq.com>
2017-06-15 06:50:38 +0800

    Add loop times

--- TransEffect/TransEffect_02.cs
+++ TransEffect/TransEffect_02.cs
@@ -6,19 +6,21 @@ using UnityEngine.UI;
 
 public class TransEffect : MonoBehaviour
 {
-    public Transform lib;
+
     public List<GoInfo> GoList;
     public float varifySpeed = 0.5f;
     public float aTime = 5f;//每个物体保持出现的时间
-    public float dTime = 5f;//第一张图片第一轮循环时出现时间
+    public float dTime = 5f;
+    public int LoopTime = 0; //循环次数
 
     private float minAlpha = 0.0f;
     private float maxAlpha = .9f;
     private float curAlpha = 1.0f;
     private float nextAlpha = 0.0f;
     private int i = 0;
+    private int looptime = 0;
 
-
+    public Transform lib;
 
     public void OnEnable()
     {
@@ -36,6 +38,7 @@ public class TransEffect : MonoBehaviour
                 go.curImg.color = c;
             
         }
+        looptime = LoopTime * 2;
     }
 
     // Update is called once per frame
@@ -58,64 +61,82 @@ public class TransEffect : MonoBehaviour
 private void Trans()
     {
 
-        Debug.Log(i);
-        GoInfo go;
-        GoInfo nextgo;
-
-        if (i >= GoList.Count - 1)
+        if (looptime >= 1)
         {
-            go = GoList[i];
-            nextgo = GoList[0]; 
-        }
-        else
-        {
-            go = GoList[i];
-            nextgo = GoList[i + 1];
-        }
+            GoInfo go;
+            GoInfo nextgo;
 
-
-        Color c = go.curImg.color;
-        Color nextc = go.curImg.color;
-
-        if (Time.time < aTime)//当前物体保持显形
-        {
-            c.a = 1;
-            go.curImg.color = c;
-        }
-        else if (Time.time >= aTime)
-        {
-            curAlpha += Time.deltaTime * varifySpeed * (-1);//当前物体逐渐消失
-            nextAlpha += Time.deltaTime * varifySpeed;//下一个物体逐渐现形
-
-            if (curAlpha <= minAlpha)//当前物体渐变到不透明时
+            if (i >= GoList.Count - 1)
             {
-                c.a = 0;//设置当前obj保持透明
+                go = GoList[i];
+                nextgo = GoList[0];
+            }
+            else
+            {
+                go = GoList[i];
+                nextgo = GoList[i + 1];
+            }
+
+
+            Color c = go.curImg.color;
+            Color nextc = go.curImg.color;
+
+            if (Time.time < aTime)//当前物体保持显形
+            {
+                c.a = 1;
                 go.curImg.color = c;
-
-                if (i == GoList.Count - 1)
-                    i = -1;
-                i++;
-                
-                //设置数据为下一物体做准备
-                curAlpha = 1;
-                nextAlpha = 0;
             }
-
-            else//当前物体逐渐透明，下一物体逐渐现形
+            else if (Time.time >= aTime)
             {
-                curAlpha = Mathf.Clamp(curAlpha, minAlpha, maxAlpha);
-                nextAlpha = Mathf.Clamp(nextAlpha, minAlpha, maxAlpha);
-                c.a = curAlpha;
-                nextc.a = nextAlpha;
-                go.curImg.color = c;
-                nextgo.curImg.color = nextc;
+                curAlpha += Time.deltaTime * varifySpeed * (-1);//当前物体逐渐消失
+                nextAlpha += Time.deltaTime * varifySpeed;//下一个物体逐渐现形
 
+                if (curAlpha <= minAlpha)//当前物体渐变到不透明时
+                {
+                    c.a = 0;//设置当前obj保持透明
+                    go.curImg.color = c;
+
+                    if (i == GoList.Count - 1)
+                        i = -1;
+                    i++;
+                    aTime = Time.time + dTime; //设置新一轮时间限制
+                    //设置数据为下一物体做准备
+                    curAlpha = 1;
+                    nextAlpha = 0;
+                }
+
+                else//当前物体逐渐透明，下一物体逐渐现形
+                {
+                    if (looptime <= 1)
+                    {
+                        Debug.Log("cur!");
+                        curAlpha = Mathf.Clamp(curAlpha, minAlpha, maxAlpha);
+                        c.a = curAlpha;
+                        go.curImg.color = c;
+                    }
+                    else
+                    {
+                        curAlpha = Mathf.Clamp(curAlpha, minAlpha, maxAlpha);
+                        nextAlpha = Mathf.Clamp(nextAlpha, minAlpha, maxAlpha);
+                        c.a = curAlpha;
+                        nextc.a = nextAlpha;
+                        go.curImg.color = c;
+                        nextgo.curImg.color = nextc;
+                    }
+                }
+
+                if (curAlpha >= maxAlpha)//下一物体完全显形
+                {
+                    
+                    if (i == GoList.Count - 1)
+                    {
+                        looptime--;
+                        Debug.Log(LoopTime);      
+                    }
+
+                }
             }
 
-            if (curAlpha >= maxAlpha)//下一物体完全显形
-            {
-                aTime = Time.time + dTime; //设置新一轮时间限制
-            }
         }
 
     }
